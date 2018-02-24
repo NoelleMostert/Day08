820bb70bf8ba9ccc76e10136fcbd3bc29334ee9f
Noelle Mostert <noellemostert@gmail.com>
2018-02-25 01:48:48 +0200

    newer with arguments

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -8,7 +8,7 @@
 # define SUCCESS	0
 # define EVEN_MSG	"I have an even number of arguments."
 # define ODD_MSG	"I have an odd number of arguments."
-# define EVEN(nbr)	(nbr % 2 ? TRUE : FALSE)
+# define EVEN(nbr)	nbr % 2 == 0
 
 void	ft_putstr(char *str);
 typedef int	t_bool;
