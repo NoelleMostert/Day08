8e3252ff580148022b6cce6f1d0b9fe7c29444b1
Noelle Mostert <noellemostert@gmail.com>
2018-02-25 00:54:49 +0200

    Day08 exercise 00 and 01

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,15 @@
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define TRUE		1
+# define FALSE		0
+# define SUCCESS	0
+# define EVEN_MSG	"I have an even number of arguments."
+# define ODD_MSG	"I have an odd number of arguments."
+# define EVEN(nbr)	(nbr % 2 ? TRUE : FALSE)
+
+void	ft_putstr(char *str);
+typedef int	t_bool;
+#endif
